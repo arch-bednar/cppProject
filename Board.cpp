bc65d5d10f325973c69c09fc318b52feb7794ea4
Marvin <dawid.bednarczyk@protonmail.com>
2021-03-28 18:59:17 +0200

    adding new classes Rectangle, Board(are not working.. yet)

---
+++ Board.cpp
@@ -0,0 +1,10 @@
+#include "Rectangle.cpp"
+
+class Board: public Rectangle
+{
+public:
+  Board(int inX, int inY, int w, int h) : Rectangle(inX, inY, w ,h){ };
+  void move(int newX){
+    x=newX;
+  }
+};
---
+++ Rectangle.cpp
@@ -0,0 +1,10 @@
+#include "Shape.cpp"
+
+class Rectangle: public Shape{
+public:
+  int width, height;
+  Rectangle(int inX, int inY, int w, int h) : Shape(inX, inY){ //x and y are left-top corner for drawning rectangle
+    width=w;
+    height=h;
+  }
+};
